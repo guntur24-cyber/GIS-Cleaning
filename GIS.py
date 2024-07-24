2b27cd4c4d200fd61568788be5e52c89cb5e893f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-07-24 16:19:45 +0700

    Update GIS.py

--- GIS.py
+++ GIS.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 import datetime as dt
 import re
-from openpyxl import Workbook
+
 
 uploaded_file = st.file_uploader("Upload File", type="xlsx")
 df_3207  = pd.read_excel(uploaded_file,header=1).fillna('')
