e51bb27f2f36d82d2b9a071850fc32de18e8a198
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-07-25 08:39:31 +0700

    Update GIS.py

--- GIS.py
+++ GIS.py
@@ -36,96 +36,97 @@ if uploaded_file is not None:
     # Baca konten zip file
 
     if st.button('Process'):
-      if selected_option=='32.07':
-        df  = pd.read_excel(uploaded_file,header=1).fillna('')
-        
-        # Find the indices of start and end markers
-        start_indices = df[df.apply(lambda row: 'Cabang :' in str(row.values), axis=1)].index
-        end_indices = df[df.apply(lambda row: 'ACCURATE Accounting System Report' in str(row.values), axis=1)].index
-        
-        # Concatenate sections into a single DataFrame
-        concatenated_df = pd.concat([df.iloc[start_idx+1:end_idx] for start_idx, end_idx in zip(start_indices, end_indices)], ignore_index=True)
-        
-        # Remove existing header
-        concatenated_df.columns = range(concatenated_df.shape[1])
-        
-        # Set the second row as the new header
-        new_header = concatenated_df.iloc[0]
-        concatenated_df = concatenated_df[1:]
-        concatenated_df.columns = new_header
-        
-        # Delete the blank Column
-        concatenated_df = concatenated_df.loc[:,['Nomor # PR','Tanggal # PR','Nomor # PO','Tanggal # PO','Pemasok','Kode #','Nama Barang','Kuantitas','@Harga','Total Harga','Rasio Satuan','Nama Satuan','Tgl/Jam Pembuatan PO#','Tgl/Jam Pembuatan PR#']]
-        
-        # Drop Unnecessary Column
-        concatenated_df = concatenated_df[concatenated_df['Nomor # PO']     !=      'Nomor # PO']
-        concatenated_df = concatenated_df[concatenated_df['Nomor # PO']     !=      '']
-        
-        
-        # Reset the index
-        concatenated_df.reset_index(drop=True, inplace=True)
-        
-        concatenated_df['Tanggal # PR']           =   pd.to_datetime(concatenated_df['Tanggal # PR'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y')
-        concatenated_df['Tanggal # PO']           =   pd.to_datetime(concatenated_df['Tanggal # PO'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y')
-        concatenated_df['Tgl/Jam Pembuatan PO#']  =   pd.to_datetime(concatenated_df['Tgl/Jam Pembuatan PO#'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y %H:%M:%S')
-        concatenated_df['Tgl/Jam Pembuatan PR#']  =   pd.to_datetime(concatenated_df['Tgl/Jam Pembuatan PR#'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y %H:%M:%S')
-        
-        
-        excel_data = to_excel(concatenated_df)
-        st.download_button(
-            label="Download Excel",
-            data=excel_data,
-            file_name='32.07.xlsx',
-            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
-
-      if selected_option=='32.15':
-        concatenated_df=[]
-        for file in uploaded_file:
-            # Read the Excel file, skipping blank lines and using the second row as the header
-            df = pd.read_excel(file, header=1).fillna('')
-        
-            # Initialize lists to store extracted dataframes
-            extracted_dfs = []
-        
-            # Find the indices of start and end markers
-            start_indices = df[df.apply(lambda row: 'Permintaan Barang' in str(row.values), axis=1)].index
-            end_indices = df[df.apply(lambda row: 'ACCURATE Accounting System Report' in str(row.values), axis=1)].index
-        
-            # Loop through each pair of start and end indices
-            for start_idx, end_idx in zip(start_indices, end_indices):
-                # Extract the desired range of rows
-                selected_rows = df.loc[start_idx:end_idx-1]
-        
+        with st.spinner('Data sedang diproses...'):
+            if selected_option=='32.07':
+                df  = pd.read_excel(uploaded_file,header=1).fillna('')
+                
+                # Find the indices of start and end markers
+                start_indices = df[df.apply(lambda row: 'Cabang :' in str(row.values), axis=1)].index
+                end_indices = df[df.apply(lambda row: 'ACCURATE Accounting System Report' in str(row.values), axis=1)].index
+                
+                # Concatenate sections into a single DataFrame
+                concatenated_df = pd.concat([df.iloc[start_idx+1:end_idx] for start_idx, end_idx in zip(start_indices, end_indices)], ignore_index=True)
+                
                 # Remove existing header
-                selected_rows.columns = range(selected_rows.shape[1])
-        
-                # Set the first row as the new header
-                new_header = selected_rows.iloc[0]
-                selected_rows = selected_rows[1:]
-                selected_rows.columns = new_header
-        
+                concatenated_df.columns = range(concatenated_df.shape[1])
+                
+                # Set the second row as the new header
+                new_header = concatenated_df.iloc[0]
+                concatenated_df = concatenated_df[1:]
+                concatenated_df.columns = new_header
+                
                 # Delete the blank Column
-                selected_rows = selected_rows.loc[:,['Permintaan Barang', 'Pesanan Pembelian', 'Penerimaan Barang', 'Uang Muka Pembelian', 'Faktur Pembelian', 'Retur Pembelian', 'Pembayaran Pembelian']]
-        
+                concatenated_df = concatenated_df.loc[:,['Nomor # PR','Tanggal # PR','Nomor # PO','Tanggal # PO','Pemasok','Kode #','Nama Barang','Kuantitas','@Harga','Total Harga','Rasio Satuan','Nama Satuan','Tgl/Jam Pembuatan PO#','Tgl/Jam Pembuatan PR#']]
+                
                 # Drop Unnecessary Column
-                selected_rows = selected_rows[selected_rows['Permintaan Barang'] != 'Permintaan Barang']
-                selected_rows = selected_rows[selected_rows['Permintaan Barang'] != '']
-        
+                concatenated_df = concatenated_df[concatenated_df['Nomor # PO']     !=      'Nomor # PO']
+                concatenated_df = concatenated_df[concatenated_df['Nomor # PO']     !=      '']
+                
+                
                 # Reset the index
-                selected_rows.reset_index(drop=True, inplace=True)
+                concatenated_df.reset_index(drop=True, inplace=True)
+                
+                concatenated_df['Tanggal # PR']           =   pd.to_datetime(concatenated_df['Tanggal # PR'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y')
+                concatenated_df['Tanggal # PO']           =   pd.to_datetime(concatenated_df['Tanggal # PO'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y')
+                concatenated_df['Tgl/Jam Pembuatan PO#']  =   pd.to_datetime(concatenated_df['Tgl/Jam Pembuatan PO#'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y %H:%M:%S')
+                concatenated_df['Tgl/Jam Pembuatan PR#']  =   pd.to_datetime(concatenated_df['Tgl/Jam Pembuatan PR#'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%d %b %Y %H:%M:%S')
+                
+                
+                excel_data = to_excel(concatenated_df)
+                st.download_button(
+                    label="Download Excel",
+                    data=excel_data,
+                    file_name='32.07.xlsx',
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                )
         
-                # Append the selected DataFrame to the list
-                extracted_dfs.append(selected_rows)
-        
-            # Concatenate all extracted dataframes
-            concatenated_df.append(pd.concat(extracted_dfs, ignore_index=True))
-            
-        concatenated_df = pd.concat(concatenated_df, ignore_index=True)
-        excel_data = to_excel(concatenated_df)
-        st.download_button(
-            label="Download Excel",
-            data=excel_data,
-            file_name='32.07.xlsx',
-            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )          
+            if selected_option=='32.15':
+                concatenated_df=[]
+                for file in uploaded_file:
+                    # Read the Excel file, skipping blank lines and using the second row as the header
+                    df = pd.read_excel(file, header=1).fillna('')
+                
+                    # Initialize lists to store extracted dataframes
+                    extracted_dfs = []
+                
+                    # Find the indices of start and end markers
+                    start_indices = df[df.apply(lambda row: 'Permintaan Barang' in str(row.values), axis=1)].index
+                    end_indices = df[df.apply(lambda row: 'ACCURATE Accounting System Report' in str(row.values), axis=1)].index
+                
+                    # Loop through each pair of start and end indices
+                    for start_idx, end_idx in zip(start_indices, end_indices):
+                        # Extract the desired range of rows
+                        selected_rows = df.loc[start_idx:end_idx-1]
+                
+                        # Remove existing header
+                        selected_rows.columns = range(selected_rows.shape[1])
+                
+                        # Set the first row as the new header
+                        new_header = selected_rows.iloc[0]
+                        selected_rows = selected_rows[1:]
+                        selected_rows.columns = new_header
+                
+                        # Delete the blank Column
+                        selected_rows = selected_rows.loc[:,['Permintaan Barang', 'Pesanan Pembelian', 'Penerimaan Barang', 'Uang Muka Pembelian', 'Faktur Pembelian', 'Retur Pembelian', 'Pembayaran Pembelian']]
+                
+                        # Drop Unnecessary Column
+                        selected_rows = selected_rows[selected_rows['Permintaan Barang'] != 'Permintaan Barang']
+                        selected_rows = selected_rows[selected_rows['Permintaan Barang'] != '']
+                
+                        # Reset the index
+                        selected_rows.reset_index(drop=True, inplace=True)
+                
+                        # Append the selected DataFrame to the list
+                        extracted_dfs.append(selected_rows)
+                
+                    # Concatenate all extracted dataframes
+                    concatenated_df.append(pd.concat(extracted_dfs, ignore_index=True))
+                    
+                concatenated_df = pd.concat(concatenated_df, ignore_index=True)
+                excel_data = to_excel(concatenated_df)
+                st.download_button(
+                    label="Download Excel",
+                    data=excel_data,
+                    file_name='32.07.xlsx',
+                    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                )          
